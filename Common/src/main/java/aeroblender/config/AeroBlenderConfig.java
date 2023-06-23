50252a50a0001300d6aab21eb4577f5a16743b87
darksonic300 <fabiross096@gmail.com>
2023-06-23 15:55:57 +0200

    Refractoring pt.2

---
+++ Common/src/main/java/aeroblender/config/AeroBlenderConfig.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) Glitchfiend
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package aeroblender.config;
+
+import java.nio.file.Path;
+
+public class AeroBlenderConfig extends ConfigFile
+{
+    public final int aetherRegionSize;
+    public final int vanillaAetherRegionWeight;
+
+    public AeroBlenderConfig(Path path)
+    {
+        super(path);
+
+        Config generalConfig = this.getSubConfig("general");
+        this.addSubConfig("General settings", "general", generalConfig);
+
+        Config generationSettings = this.getSubConfig("generation_settings");
+        this.aetherRegionSize = generationSettings.addNumber("The size of aether biome regions from each mod that uses AtumBlender.", "aether_region_size", 2, 2, 6);
+        this.vanillaAetherRegionWeight = generationSettings.addNumber("The weighting of vanilla biome regions in the aether.", "vanilla_aether_region_weight", 10, 0, Integer.MAX_VALUE);
+        this.addSubConfig("Generation settings", "generation_settings", generationSettings);
+
+        this.save();
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,6 @@
-<p align="center"><img src="https://i.imgur.com/8VBgnKN.png"></p>
 
-<p align="center"><img src="https://i.imgur.com/CYxKg5M.png"></p>
 
-<p align="center">https://discord.gg/GyyzU6T</p>
-
-**TerraBlender** is a **library mod** for adding biomes in a simple and compatible manner with Minecraft's new biome/terrain system.
+**AeroBlender** is a **library mod** for adding biomes in a simple and compatible manner with Aether's biome/terrain system.
 
 -----------------
 
