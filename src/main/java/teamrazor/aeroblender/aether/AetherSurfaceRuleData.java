6a692b21bd93fc298d6325474eec7e4b0ee99b25
darksonic300 <fabiross096@gmail.com>
2023-08-06 00:21:20 +0200

    Text Fix: Renaming

--- src/main/java/teamrazor/aeroblender/aether/AetherSurfaceRuleData.java
+++ src/main/java/teamrazor/aeroblender/aether/AetherSurfaceRuleData.java
@@ -30,15 +30,15 @@ import terrablender.api.SurfaceRuleManager;
 
 //Default SurfaceRules of Aether
 public class AetherSurfaceRuleData {
-    private static final SurfaceRules.RuleSource GRASS_BLOCK = makeStateRule(AetherBlocks.AETHER_GRASS_BLOCK.get());
-    private static final SurfaceRules.RuleSource DIRT = makeStateRule(AetherBlocks.AETHER_DIRT.get());
+    private static final SurfaceRules.RuleSource AETHER_GRASS_BLOCK = makeStateRule(AetherBlocks.AETHER_GRASS_BLOCK.get());
+    private static final SurfaceRules.RuleSource AETHER_DIRT = makeStateRule(AetherBlocks.AETHER_DIRT.get());
 
     private static SurfaceRules.RuleSource makeStateRule(Block block) {
         return SurfaceRules.state(block.defaultBlockState().setValue(AetherBlockStateProperties.DOUBLE_DROPS, true));
     }
 
     public static SurfaceRules.RuleSource aether() {
-         SurfaceRules.RuleSource surface = SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.waterBlockCheck(-1, 0), GRASS_BLOCK), DIRT);
-         return SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, surface), SurfaceRules.ifTrue(SurfaceRules.UNDER_FLOOR, DIRT));
+         SurfaceRules.RuleSource surface = SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.waterBlockCheck(-1, 0), AETHER_GRASS_BLOCK), AETHER_DIRT);
+         return SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, surface), SurfaceRules.ifTrue(SurfaceRules.UNDER_FLOOR, AETHER_DIRT));
     }
 }
