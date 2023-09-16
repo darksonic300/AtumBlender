183ce3f8cf54a11cc9bdfd3b93db4d33a6989ec7
darksonic300 <fabiross096@gmail.com>
2023-09-16 17:27:39 +0200

    Port: Registry fix

--- src/main/java/teamrazor/aeroblender/DimensionTypeTags.java
+++ src/main/java/teamrazor/aeroblender/DimensionTypeTags.java
@@ -10,6 +10,6 @@ public class DimensionTypeTags {
     public static final TagKey<DimensionType> AETHER_REGIONS = create("aether_regions");
 
     private static TagKey<DimensionType> create(String id) {
-        return TagKey.create(Registry.f_122818_, new ResourceLocation(TerraBlender.MOD_ID, id));
+        return TagKey.create(Registry.DIMENSION_TYPE_REGISTRY, new ResourceLocation(TerraBlender.MOD_ID, id));
     }
 }
