e3f18580759ccefa3c6dbb6a76050e3c0605e7c4
darksonic300 <fabiross096@gmail.com>
2024-04-17 14:29:01 +0200

    Port: Some more

--- src/main/java/teamrazor/aeroblender/Aeroblender.java
+++ src/main/java/teamrazor/aeroblender/Aeroblender.java
@@ -2,13 +2,15 @@ package teamrazor.aeroblender;
 
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.world.level.levelgen.SurfaceRules;
+import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.neoforge.common.NeoForge;
 import teamrazor.aeroblender.mixin.SurfaceRuleManagerAccessor;
 import terrablender.api.SurfaceRuleManager;
+import terrablender.handler.InitializationHandler;
 import terrablender.worldgen.surface.NamespacedSurfaceRuleSource;
 
 @Mod(Aeroblender.MODID)
@@ -16,8 +18,7 @@ public class Aeroblender
 { public static final String MODID = "aeroblender";
     public Aeroblender()
     {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        //NeoForge.EVENT_BUS.register(this);
+        NeoForge.EVENT_BUS.register(this);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, AeroBlenderConfig.COMMON_SPEC);
     }
 
