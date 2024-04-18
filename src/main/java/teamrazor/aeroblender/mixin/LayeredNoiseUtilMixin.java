893af503ba861ef539d46df53e2f7679ec5a827c
TunefulTurnip <tore.ekerhult@gmail.com>
2024-04-18 11:05:55 +0200

    Finish 1.20.4 port

--- src/main/java/teamrazor/aeroblender/mixin/LayeredNoiseUtilMixin.java
+++ src/main/java/teamrazor/aeroblender/mixin/LayeredNoiseUtilMixin.java
@@ -19,30 +19,17 @@ import java.util.function.LongFunction;
 @Mixin(value = LayeredNoiseUtil.class, remap = false)
 public abstract class LayeredNoiseUtilMixin {
 
-    @Inject(at = @At("HEAD"), method = "uniqueness", cancellable = true)
-    private static void uniqueness(RegistryAccess registryAccess, RegionType regionType, long worldSeed, CallbackInfoReturnable<Area> cir) {
-        if(regionType == AetherRegionType.THE_AETHER) {
-            int numZooms1 = AeroBlenderConfig.COMMON.aetherRegionSize.get();
-
-            LongFunction<AreaContext> contextFactory = (seedModifier) -> new AreaContext(25, worldSeed, seedModifier);
-            AreaFactory factory = new InitialLayer(registryAccess, regionType).run(contextFactory.apply(1L));
-            factory = ZoomLayer.FUZZY.run(contextFactory.apply(2000L), factory);
-            factory = zoom(2001L, ZoomLayer.NORMAL, factory, 3, contextFactory);
-            factory = zoom(1001L, ZoomLayer.NORMAL, factory, numZooms1, contextFactory);
-            cir.setReturnValue(factory.make());
-            cir.cancel();
-        }
+    @Shadow
+    public static Area createZoomedArea(long seed, int zooms, AreaTransformer0 initialTransformer) {
+        return null;
     }
 
-    @Shadow
-    public static AreaFactory zoom(long seedModifier, AreaTransformer1 transformer, AreaFactory initialAreaFactory, int times, LongFunction<AreaContext> contextFactory) {
-        AreaFactory areaFactory = initialAreaFactory;
-
-        for(int i = 0; i < times; ++i) {
-            areaFactory = transformer.run(contextFactory.apply(seedModifier + (long)i), areaFactory);
+    @Inject(at = @At("HEAD"), method = "uniqueness", cancellable = true)
+    private static void uniqueness(RegistryAccess registryAccess, RegionType regionType, long seed, CallbackInfoReturnable<Area> cir) {
+        if (regionType == AetherRegionType.THE_AETHER) {
+            int numZooms1 = AeroBlenderConfig.COMMON.aetherRegionSize.get();
+            cir.setReturnValue(createZoomedArea(seed, numZooms1, new InitialLayer(registryAccess, regionType)));
         }
-
-        return areaFactory;
     }
 }
 
---
+++ src/main/java/teamrazor/aeroblender/mixin/NoiseGeneratorSettingsMixin.java
@@ -0,0 +1,42 @@
+package teamrazor.aeroblender.mixin;
+
+
+import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
+import net.minecraft.world.level.levelgen.SurfaceRules;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import teamrazor.aeroblender.Aeroblender;
+import teamrazor.aeroblender.aether.AetherRuleCategory;
+import terrablender.api.SurfaceRuleManager;
+import terrablender.worldgen.IExtendedNoiseGeneratorSettings;
+
+@Mixin(value = NoiseGeneratorSettings.class, priority = 900)
+public class NoiseGeneratorSettingsMixin implements IExtendedNoiseGeneratorSettings {
+
+    @Shadow
+    private SurfaceRules.RuleSource surfaceRule;
+
+    @Unique
+    private SurfaceRuleManager.RuleCategory ruleCategory = null;
+    @Unique
+    private SurfaceRules.RuleSource namespacedSurfaceRuleSource = null;
+
+   @Inject(method = "surfaceRule", at = @At("HEAD"), cancellable = true)
+   private void surfaceRule(CallbackInfoReturnable<SurfaceRules.RuleSource> cir) {
+       if (this.ruleCategory == AetherRuleCategory.THE_AETHER) {
+           if (this.namespacedSurfaceRuleSource == null)
+               this.namespacedSurfaceRuleSource = Aeroblender.getAetherNamespacedRules(AetherRuleCategory.THE_AETHER, this.surfaceRule);
+           cir.setReturnValue(this.namespacedSurfaceRuleSource);
+       }
+   }
+
+    @Override
+    public void setRuleCategory(SurfaceRuleManager.RuleCategory ruleCategory) {
+        this.ruleCategory = ruleCategory;
+    }
+}
