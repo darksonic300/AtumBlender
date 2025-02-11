02e426bf420b1e3c39fba43aed1ec40ce9a3007e
VoidLeech <ian.gerritse@gmail.com>
2025-02-11 20:51:39 +0100

    fix: incompat with terrablender's end backport

--- src/main/java/teamrazor/aeroblender/mixin/NoiseGeneratorSettingsMixin.java
+++ src/main/java/teamrazor/aeroblender/mixin/NoiseGeneratorSettingsMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -12,6 +13,7 @@ import teamrazor.aeroblender.Aeroblender;
 import teamrazor.aeroblender.aether.AetherRegionType;
 import teamrazor.aeroblender.aether.AetherRuleCategory;
 import terrablender.api.RegionType;
+import terrablender.api.SurfaceRuleManager;
 import terrablender.worldgen.IExtendedNoiseGeneratorSettings;
 
 @Mixin(value = NoiseGeneratorSettings.class, priority = 900)
@@ -20,13 +22,15 @@ public class NoiseGeneratorSettingsMixin implements IExtendedNoiseGeneratorSetti
     @Shadow
     private SurfaceRules.RuleSource surfaceRule;
 
-    private RegionType regionType  = null;
+    @Unique
+    private SurfaceRuleManager.RuleCategory ruleCategory = null;
 
+    @Unique
     private SurfaceRules.RuleSource namespacedSurfaceRuleSource = null;
 
    @Inject(method = "surfaceRule", at = @At("HEAD"), cancellable = true)
    private void surfaceRule(CallbackInfoReturnable<SurfaceRules.RuleSource> cir) {
-       if (this.regionType == AetherRegionType.THE_AETHER) {
+       if (this.ruleCategory == AetherRuleCategory.THE_AETHER) {
            if (this.namespacedSurfaceRuleSource == null)
                this.namespacedSurfaceRuleSource = Aeroblender.getAetherNamespacedRules(AetherRuleCategory.THE_AETHER, this.surfaceRule);
            cir.setReturnValue(this.namespacedSurfaceRuleSource);
@@ -34,14 +38,7 @@ public class NoiseGeneratorSettingsMixin implements IExtendedNoiseGeneratorSetti
    }
 
     @Override
-    public void setRegionType(RegionType regionType)
-    {
-        this.regionType = regionType;
-    }
-
-    @Override
-    public RegionType getRegionType()
-    {
-        return this.regionType ;
+    public void setRuleCategory(SurfaceRuleManager.RuleCategory ruleCategory) {
+        this.ruleCategory = ruleCategory;
     }
 }
